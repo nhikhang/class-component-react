f9cdcff7beef4e0d73ac360ce2dc92cef230c036
nhikhang <nhikhang1309@gmail.com>
2022-10-23 15:36:51 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Count from './components/Count';
 import './App.css';
+import {MASTER_DATA} from './ultis/constants';
 
 class App extends React.Component {
     constructor() {
@@ -48,68 +49,16 @@ class App extends React.Component {
                     decrement={this.handleDecrement} 
                     changeText={this.handleChangeText}   
                 />
+
+                <ul>
+                    {
+                        MASTER_DATA.length > 0 ? MASTER_DATA.map((item, index) => (
+                            <li key={item.name}>{item.name} {item.ver}</li>
+                        )) : <p className="noneData">No Data</p>
+                    }
+                </ul>
             </div>
         )
     }
 }
 export default App;
-
-
-
-// import './App.css';
-// import React from 'react';
-// import Count from './components/Count'
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       count: 0,
-//       text: 'React'
-//     }
-//   }
-
-//   handleIncrement = () => {
-//     this.setState({
-//       ...this.state,
-//       count: this.state.count + 1
-//     });
-//   }
-
-//   handleDecrement = () => {
-//     this.setState({
-//       ...this.state,
-//       count: this.state.count - 1
-//     });
-//   }
-
-//   handleChangeText = (props) => {
-//     this.setState({
-//       ...this.state,
-//       text: props.target.value
-//     });
-//   }
-
-//   render() {
-//     const { count, text } = this.state;
-
-//     return (
-//       <div className="App">
-//         <h2>Class Component</h2>
-//         <>
-//           <p>{count}</p>
-//           <p>{text}</p>
-//         </>
-//         < Count 
-//             number={count} 
-//             increment={this.handleIncrement} 
-//             decrement={this.handleDecrement}
-//             changeText={this.handleChangeText}
-//         />
-//       </div>
-//     )
-//   }
-// }
-
-// export default App;
---
+++ src/ultis/constants.js
@@ -0,0 +1,18 @@
+export const MASTER_DATA = [
+    {
+        name: 'React',
+        ver: 18
+    },
+    {
+        name: 'Angular',
+        ver: 12
+    },
+    {
+        name: 'Nodejs',
+        ver: 16
+    },
+    {
+        name: 'Nextjs',
+        ver: 11
+    }
+];
